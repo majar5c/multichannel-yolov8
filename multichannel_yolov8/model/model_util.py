b87c0ffd8ecbe5b83a8dc131b52186d84198a61c
Jason Chen <jche774@aucklanduni.ac.nz>
2023-09-05 11:33:04 +1200

    fix: Allow model warmup in GPU mode

--- multichannel_yolov8/model/model_util.py
+++ multichannel_yolov8/model/model_util.py
@@ -205,7 +205,7 @@ class MultiChannelPredictor(SegmentationPredictor):
 
         # Warmup model
         if not self.done_warmup:
-            self.model.warmup(imgsz=(1 if self.model.pt or self.model.triton else self.dataset.bs, 3, *self.imgsz))
+            self.model.warmup(imgsz=(1 if self.model.pt or self.model.triton else self.dataset.bs, next(model.named_parameters())[1].shape[1], *self.imgsz))
             self.done_warmup = True
 
         self.seen, self.windows, self.batch, profilers = 0, [], None, (ops.Profile(), ops.Profile(), ops.Profile())
